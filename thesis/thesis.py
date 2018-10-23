c45eea41a185a86da8d4ed59e44f143eac48c655
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-10-24 00:28:51 +0300

    histogram plot for the distributon of point density and spacing

--- thesis/thesis.py
+++ thesis/thesis.py
@@ -6,27 +6,51 @@ from lowo.extract_lidar_info import get_density_spacing_info as ex
 import pandas as pd
 data = pd.read_csv('./lidar_info_extracted.csv')
 
-# skip the first column
+# skip the first two column
 data=data.iloc[:, 2:] 
 # ex()
 
 
 
-data.describe().to_csv('output_data/2015_lidar_info.csv')
-print(data)
+
+data.describe().to_csv('output_data/2015_lidar_info_stat.csv')
 fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12,8))
 
+ax11=axes[0, 0]
+ax12=axes[0, 1]
+ax21=axes[1, 0]
+ax22=axes[1, 1]
 
+# fig.tight_layout()
+plt.suptitle('Distribution of point density and spacing of the 2015 LIDAR Data')
 # # Make default histogram of sepal length
-sns.distplot( data['all_returns_density'] , ax=axes[0, 0])
-sns.distplot( data['last_returns_density'] , ax=axes[0, 1]) 
-sns.distplot( data['all_returns_spacing'] , ax=axes[1, 0])
-sns.distplot( data['last_returns_spacing'] , ax=axes[1, 1])
-
-plt.show() 
+sns.distplot( data['all_returns_density'] , ax=ax11, bins=20 )
+sns.distplot( data['last_returns_density'] , ax=ax12, bins=20) 
+sns.distplot( data['all_returns_spacing'] , ax=ax21, bins=20)
+sns.distplot( data['last_returns_spacing'] , ax=ax22, bins=20)
+# plt.legend()
+ax11.set(xlabel='all returns', ylabel='point density(points/sqm)')
+ax12.set(xlabel='last returns', ylabel='')
+ax21.set(xlabel='all returns', ylabel='point spacity(points/sqm)')
+ax22.set(xlabel='last returns', ylabel='')
+plt.savefig('./output_data/spacing_density2015.png')
  
+# plt.show(block=True)    
+   
 # Control the number of bins
-sns.distplot( data['all_returns_density'] , bins=20 )
+
+fig, ax = plt.subplots()
+
+# hide axes
+fig.patch.set_visible(False)
+ax.axis('off')
+ax.axis('tight')
+
+ax.table(cellText=data.describe().round(decimals=2).values, colLabels=data.describe().columns, loc='center')
+
+fig.tight_layout()
+# plt.savefig('./output_data/stat_2015.png')
+
 # plt.show()
 
 
