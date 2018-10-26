3253f360f93d6a709c58f8e29099d79206ee3061
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-10-26 22:09:22 +0300

    unused codes cleaned from analysis

--- thesis/analysis.py
+++ thesis/analysis.py
@@ -1,67 +1,42 @@
 # =============================================================================
-# import rasterio
-# from rasterio.plot import show
-# from rasterio.plot import show_hist
-# from rasterio.mask import mask
-# from shapely.geometry import box
-# import geopandas as gpd
-# from fiona.crs import from_epsg
-# import pycrs
-# import matplotlib.pyplot as plt
-# from osgeo import gdal
-# import utm
-# from rasterToPolygon import polygonize
-# import clip_raster
+ from rasterio.plot import show
+ from rasterio.plot import show_hist
+ from rasterio.mask import mask
+ from shapely.geometry import box
+ import geopandas as gpd
+ from fiona.crs import from_epsg
+ import pycrs
+ import matplotlib.pyplot as plt
+ from osgeo import gdal
+ import utm
+ from rasterToPolygon import polygonize
+ import clip_raster
 # =============================================================================
 
-
+import rasterio
+from rasterio.plot import show
+from rasterio.plot import show_hist
+from rasterio.mask import mask
 from rasterToPolygon import polygonize
 import clip_raster as ras
 from make_grid import create_grid
 import geopandas as gpd
+from shapely.geometry import box
 
 #readthe shapefile for the area of interest
 aoi_shapefile = gpd.read_file(r'E:\LIDAR_FINAL\data\AOI\fishnet_926_1sqm.shp')
 
-bbox_aoi = ras.get_vector_extent(aoi_shapefile)
+bbox_aoi2 = ras.get_vector_extent(aoi_shapefile)
+bbox_aoi = ras.get_raster_extent(r'E:\LIDAR_FINAL\data\AOI\clipped_mean_annual_rain.tif')
+#[38.19986023835, -3.2418059025499986, 38.52486023705, -3.516805901449999]
 
 
-#read and clip the rainfall data
-#MEAN_ANNUAL
-mean_annual = 
-
-data = rasterio.open(fp)
-fishnet = gpd.read_file(grid_path)
-
-fishnet.crs
-
-#import gdal
-
-os.getcwd()
-
-
-fp= r'E:\LIDAR_FINAL\data\precipitation\mean_annual\mean_annual_rainfall_clipped.tif'
-out_tif=r'E:\LIDAR_FINAL\data\precipitation\mean_annual\test_lowo.tif'
-
-
-grid_path = r'E:\LIDAR_FINAL\data\2015\fishnet\fishnet_925_1sqm.shp'
-test = r'E:\LIDAR_FINAL\data\precipitation\mean_annual\CHELSA_bio_12.tif'
-ll = clip_raster.main(grid_path, test)
-
-data = rasterio.open(fp)
-print(data)
-show((data, 1), cmap='terrain')
-# plt.show()
-fishnet = gpd.read_file(grid_path)
+mean_annual_filepath = r'E:\LIDAR_FINAL\data\precipitation\mean_annual\CHELSA_bio_12.tif'
+mean_annual_clipped_path = r'E:\LIDAR_FINAL\data\precipitation\mean_annual\mean_annual_rain_cli.tif'
+mean_annual_rain_raster = rasterio.open(mean_annual_filepath)
+#clip the mean annual rainfall raster data
+mean_annual_rain_clipped = ras.get_clipped_raster(mean_annual_rain_raster, mean_annual_clipped_path,
+                                                  bbox_aoi, 4326)
 
 
 
-print(data.crs)
-print(fishnet.crs)
-polygonized_raster = polygonize(fp, data.crs, 32737)
-polygonized_raster.plot(column='raster_val', linewidth=0.03, cmap="Blues", scheme="equal_interval", k=9, alpha=0.9)
-
-
-polygonized_raster.to_file('lowo.shp')
-# show((data, 1), cmap='terrain')
-# plt.show()
