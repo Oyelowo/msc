d62bb82b7ab9246b1913bb05bb8fc77b82d2e1e9
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-10-27 14:29:05 +0300

    generating grid based on shapefile extent or bounding box

--- thesis/analysis.py
+++ thesis/analysis.py
@@ -85,28 +85,20 @@ show(sum_rain,cmap='Blues', title="Mean Annual Rainfall")
 
 
 # CREATE A FISHNET/GRID OF 926.1m PIXEL
-#For bounding box in longitude and latitude
-grid = ras.create_grid(926.1, 926.1, bbox_aoi, is_utm=True)
-
-
-
-def bbox_to_utm(bbox, zone_number):
-    lonlow, lathigh, lonhigh, latlow = bbox
-    minx, maxy,*others = utm.from_latlon(lathigh, lonlow, zone_number)
-    maxx, miny, *others = utm.from_latlon(latlow, lonhigh, zone_number)
-    return [minx, maxy , maxx, miny]
-
-#minx, maxy , maxx, miny
-
-aoi_shapefile.total_bounds
-#For bounding box in UTM Zone
+#generating grid by directly providing the bounding box
 grid = ras.create_grid(926.1, 926.1, bbox_aoi, is_utm=False)
-grid = ras.create_grid(926.1, 926.1, bbox_aoi2, is_utm=False)
+
+#generating grid based on shapefile extent
+grid2 = ras.create_grid(926.1, 926.1, shapefile=aoi_shapefile)
 
 #grid = ras.create_grid(gridHeight=926.1, gridWidth=926.1,shapefile=aoi_shapefile)
 grid.plot()
 
 grid_path = r'E:\LIDAR_FINAL\data\grid\grid.shp'
 grid.to_file(grid_path)
+
+
+
+
 
 
