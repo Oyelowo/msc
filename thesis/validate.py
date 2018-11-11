94747ce2150ad4a68f452a027f1da4d1cafb2723
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-11-11 08:21:45 +0200

    accuracy analysis for error of omission done

--- thesis/validate.py
+++ thesis/validate.py
@@ -6,37 +6,15 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 from shapely.geometry import Polygon,Point
 
+# =============================================================================
+# IMPORT DATA
+# =============================================================================
 digitized_roof = gpd.read_file(r'E:\LIDAR_FINAL\diigised_Samples_roofs\building\buildings\new_buildings.shp')
 roof_2013 = gpd.read_file(r'E:\LIDAR_FINAL\2013\buildings\buildings_2013_projected_regularized.shp')
 roof_2015 = gpd.read_file(r'E:\LIDAR_FINAL\data\2015\buildings\buildings_2015_simplified.shp')
 aoi = gpd.read_file(r'E:\LIDAR_FINAL\diigised_Samples_roofs\aoi_roof_samples.shp')
 
 
-
-# =============================================================================
-# SELECT ONLY ROOFS WITHIN THE AOI
-# =============================================================================
-aoi_boundary = aoi.loc[0].geometry
-type(aoi_boundary)
-roof_2013 = roof_2013[roof_2013.geometry.within(aoi_boundary)]
-roof_2013.plot()
-roof_2015 = roof_2015[roof_2015.geometry.within(aoi_boundary)]
-roof_2015.plot()
-
-
-len(roof_2013)
-len(roof_2015)
-len(digitized_roof)
-# =============================================================================
-# 
-# =============================================================================
-
-del digitized_roof['buildings']
-
-
-bbox = roof_2013.total_bounds
-digitized_roof['area'] = digitized_roof['geometry'].area
-digitized_roof['ID'] = digitized_roof.index + 1
 
 # =============================================================================
 # GET THE BOUDING BOX OF THE DIGITISED ROOFS AND MAKE THE AOI OUT OF IT
@@ -48,30 +26,56 @@ aoi = gpd.GeoDataFrame(gpd.GeoSeries(pp), columns=['geometry'])
 aoi.plot()
 
 
+# =============================================================================
+# SELECT ONLY ROOFS WITHIN THE AOI
+# =============================================================================
+aoi_boundary = aoi.loc[0].geometry
+type(aoi_boundary)
+roof_2013 = roof_2013[roof_2013.geometry.within(aoi_boundary)]
+roof_2013.plot()
+roof_2015 = roof_2015[roof_2015.geometry.within(aoi_boundary)]
+roof_2015.plot()
+len(roof_2013)
+len(roof_2015)
+len(digitized_roof)
 
 
+# =============================================================================
+# GET AREA AND CREATE ID FOR EACH POLYGON OF THE DIGITISED ROOFS
+# =============================================================================
+del digitized_roof['buildings']
+digitized_roof['area'] = digitized_roof['geometry'].area
+digitized_roof['ID'] = digitized_roof.index + 1
 
 
-
+# =============================================================================
+# FILTER TOO SMALL OR TOO BIG POLYGONS
+# =============================================================================
+lower_limit, upper_limit = 10, 2000
 roof_2013['area'] = roof_2013.geometry.area
-roof_2013 = roof_2013.loc[(roof_2013['area']>10) & (roof_2013['area']<2000)].reset_index(drop=True)
+roof_2013 = roof_2013.loc[(roof_2013['area']>lower_limit) & (roof_2013['area']<upper_limit)].reset_index(drop=True)
 
 roof_2015['area'] = roof_2015.geometry.area
-roof_2015 = roof_2015.loc[(roof_2015['area']>10) & (roof_2015['area']<2000)].reset_index(drop=True)
+roof_2015 = roof_2015.loc[(roof_2015['area']>lower_limit) & (roof_2015['area']<upper_limit)].reset_index(drop=True)
 
 
+# =============================================================================
+# Check the data
+# =============================================================================
 roof_2013.plot(column='area', cmap="RdBu", scheme="quantiles", alpha=0.9)
 #del digitized_roof['buildings']
 roof_2013.isna().sum()
 digitized_roof.isna().sum()
 
-roof_2013['geometry'] = roof_2013['geometry']
 digitized_roof['geometry'][1]
 roof_2013['geometry'][3]
 
 roof_2013r = roof_2013.loc[6:12,:]
 print(roof_2013.loc[12,'geometry'])
 roof_2013.loc[14,'geometry'].exterior
+
+
+
 
 poly1 = digitized_roof.copy()
 poly2 = roof_2013.copy()
@@ -92,35 +96,60 @@ len(kkn)
 ((500 - 412) * 100)/ 500
 
 
-
+# =============================================================================
+# INTERSECTING THE DIGITISED AND THE BUILDINGS EXTRACTED FROM LIDAR
+# =============================================================================
 #poly1_c = poly1[['geometry', 'ID']]
-poly1_geom, poly1_ID = poly1['geometry'], poly1['ID']
-poly2_geom, poly2_ID = poly2['geometry'], poly2['ID']
-list(zip(poly1_geom, poly1_ID))
+digi_geom, digi_ID, poly1_area = digitized_roof['geometry'], digitized_roof['ID'], digitized_roof['area']
+roof_2013_geom, roof_2013_area = roof_2013['geometry'], roof_2013['area']
+roof_2015_geom, roof_2015_area = roof_2015['geometry'], roof_2015['area']
 
-poly1_c = list(zip(poly1['geometry'], poly1['ID']))
-poly2_c = list(zip(poly2['geometry'], poly2['ID']))
+digi_zip_list = list(zip(digi_geom, digi_ID, poly1_area))
+roof_2013_zip_list = list(zip(roof_2013_geom, roof_2013_area))
+roof_2015_zip_list = list(zip(roof_2015_geom, roof_2015_area))
 
-poly2_list = poly2['geometry'].tolist()
-h = gpd.GeoDataFrame()
-del intersected
-for i, (orig_geom, orig_ID) in enumerate(poly1_c):
-  intersected = [{'geometry': ref_geom.intersection(orig_geom), 'orig_ID': orig_ID}
-  for ref_geom in poly2_list if ref_geom.intersection(orig_geom)]
+digi_intersect_roof_2013=[]
+digi_intersect_roof_2015=[]
+for i, (orig_geom, orig_ID, orig_area) in enumerate(digi_zip_list):
+  for ref_geom, ref_area in roof_2013_zip_list:
+    if ref_geom.intersection(orig_geom):
+      geom = ref_geom.intersection(orig_geom)
+      digi_intersect_roof_2013.append({'geometry': geom, 'digi_ID': orig_ID, 'lidar_area':orig_area, 'ref_area': ref_area})
+      
+  for ref_geom, ref_area in roof_2015_zip_list:
+    if ref_geom.intersection(orig_geom):
+      geom = ref_geom.intersection(orig_geom)
+      digi_intersect_roof_2015.append({'geometry': geom, 'digi_ID': orig_ID,'lidar_area':orig_area, 'ref_area': ref_area})
   print('Checking:', orig_ID)
+      
+
+digi_inter_roof13_df = gpd.GeoDataFrame(digi_intersect_roof_2013)
+digi_inter_roof15_df = gpd.GeoDataFrame(digi_intersect_roof_2015)
 
 
-intersected
 
-for i, (orig_geom, orig_ID) in enumerate(zip(poly1_geom, poly1_ID)):   
-   print(orig_ID)
+# =============================================================================
+# ACCURACY ANALYSIS
+# =============================================================================
+digi_roofs_count = len(digitized_roof)
+
+#2013
+digi_inter_roof13_df_grouped = digi_inter_roof13_df.groupby('orig_ID')
+correct_roof2013_count = len(digi_inter_roof13_df_grouped)
+omission_roof_2013 = ((digi_roofs_count - correct_roof2013_count) *100)/ digi_roofs_count
+
+print('The error of ommission for 2013 extraction is {0}%'.format(omission_roof_2013))
+print('Accuracy is {0}%'.format(100-omission_roof_2013))
+
+#2015
+digi_inter_roof15_df_grouped = digi_inter_roof15_df.groupby('orig_ID')
+correct_roof2015_count = len(digi_inter_roof15_df_grouped)
+omission_roof_2015 = ((digi_roofs_count - correct_roof2015_count) *100)/ digi_roofs_count
+
+print('The error of ommission for 2013 extraction is {0}%'.format(omission_roof_2015))
+print('Accuracy is {0}%'.format(100-omission_roof_2015))
+
+digi_inter_roof13_df_grouped.geometry.agg(lambda x: x.centroid)
 
 
-intersection = gpd.overlay(digitized_roof, roof_2015, how='intersection')
-
-#roof_2013 = roof_2013.dissolve(by='area')
-
-digitized_roof.exterior
-roof_2013.exterior
-
-print(roof_2013.geom_type)
+digi_inter_roof13_df.geometry[10]
